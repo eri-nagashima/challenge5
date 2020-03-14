cc76d7e2b6e06d3688bbd8b1f02e8ea679c1a364
Eri Nagashima <erinagashima@Eri-Nagashima.local>
2020-03-14 14:49:56 +0900

    Update

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-function addRedframe() {
-    const redframe = document.querySelector(".key");
+function addFrameSound(event) {
+    const redframe = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    const sounds = document.querySelector(`audio[data-key="${event.keyCode}"]`);
     redframe.classList.add("playing");
+    sounds.play();
+    sounds.currentTime = 0;
   }
-addRedframe();
+window.addEventListener("keydown", addFrameSound);
+
+
+function removeFrameSound() {
+    const removeredframe = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    removeredframe.classList.remove("playing");
+}
+window.addEventListener("keyup", removeFrameSound);
