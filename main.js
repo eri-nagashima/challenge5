5620f9ea760e1b9274c00250af96138424314b67
Eri Nagashima <erinagashima@Eri-Nagashima.local>
2020-04-11 16:14:06 +0900

    update

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function addFrameSound(event) {
     const redframe = document.querySelector(`.key[data-key="${event.keyCode}"]`);
     const sounds = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-    redframe.classList.add("playing");
+    redframe.classList.add('playing');
     sounds.play();
     sounds.currentTime = 0;
   }
@@ -9,7 +9,12 @@ window.addEventListener("keydown", addFrameSound);
 
 
 function removeFrameSound() {
-    const removeredframe = document.querySelector(`.key[data-key="${event.keyCode}"]`);
-    removeredframe.classList.remove("playing");
+    const removeredframe = document.getElementsByClassName("key");
+    for (let div of removeredframe) {
+      div.classList.remove('playing'); // クラスを取り除く
+    }  
 }
 window.addEventListener("keyup", removeFrameSound);
+
+//how...どういう風に消したいか
+//when/where/howを気にする
